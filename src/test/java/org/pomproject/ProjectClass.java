9d741e6054f28fc54b505a36d21907473e52a667
karthiksk6006 <karthiksk627857@gmail.com>
2023-03-24 10:27:08 +0530

    karthik modify

--- src/test/java/org/pomproject/ProjectClass.java
+++ src/test/java/org/pomproject/ProjectClass.java
@@ -6,5 +6,9 @@ import org.openqa.selenium.support.PageFactory;
 public class ProjectClass extends BaseClass {
 public ProjectClass() {
 	PageFactory.initElements(driver, this);
+	
+	System.out.println("its me karthik here");
+	System.out.println("1111111111111111111");
+	
 }
 }
