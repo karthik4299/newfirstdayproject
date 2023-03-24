c308dfedd12fea29b1350f09bb3bac45e6d48a3a
karthik4299 <karthikkumarkarthi42@gmail.com>
2023-03-24 18:08:28 +0530

    karthik42 new code

--- src/test/java/or/task/Sample.java
+++ src/test/java/or/task/Sample.java
@@ -8,9 +8,12 @@ System.out.println("karthik2");
 System.out.println("karthik3");
 System.out.println("karthik4");
 
+
 System.out.println("master 1");
 System.out.println("master 1");
-System.out.println("master 1");
+System.out.println("master 2");
+System.out.println("master 3");
+System.out.println("master 4");
 
 	}
 
