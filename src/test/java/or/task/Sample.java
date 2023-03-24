9fb55197e9b6bc36acce03739e238e7196f90039
karthik4299 <karthikkumarkarthi42@gmail.com>
2023-03-24 18:01:57 +0530

    karthik42 new

--- src/test/java/or/task/Sample.java
+++ src/test/java/or/task/Sample.java
@@ -8,6 +8,10 @@ System.out.println("karthik2");
 System.out.println("karthik3");
 System.out.println("karthik4");
 
+System.out.println("master 1");
+System.out.println("master 1");
+System.out.println("master 1");
+
 	}
 
 }
