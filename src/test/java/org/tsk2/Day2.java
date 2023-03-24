5f510935c2d433aba728fc4f62162bee19e45961
priyadevi12 <priyadevidinesh7@gmail.com>
2023-03-24 13:12:11 +0530

    day2 task

---
+++ src/test/java/org/tsk2/Day2.java
@@ -0,0 +1,10 @@
+package org.tsk2;
+
+public class Day2 {
+	public static void main(String[] args) {
+		System.out.println("task1");
+		System.out.println("task2");
+		System.out.println("task3");
+	}
+
+}
