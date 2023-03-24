70f3c56e6f751592ec4fcba49eafc5991a4060fd
balasudhan23 <balasudhan2000@gmail.com>
2023-03-24 10:51:18 +0530

    bala modify

--- src/test/java/org/pomproject/ProjectClass.java
+++ src/test/java/org/pomproject/ProjectClass.java
@@ -1,10 +1,15 @@
 package org.pomproject;
 
+import java.util.concurrent.TimeUnit;
+
 import org.base.BaseClass;
 import org.openqa.selenium.support.PageFactory;
 
 public class ProjectClass extends BaseClass {
 public ProjectClass() {
 	PageFactory.initElements(driver, this);
+	driver.manage().window().maximize();
+
+	
 }
 }
