4607e3b3b058e4112c13cef3ec85a558c3d0ef43
karthiksk6006 <karthiksk627857@gmail.com>
2023-03-24 12:20:34 +0530

    karthik modify

---
+++ src/test/java/org/DEMO/Class.java
@@ -0,0 +1,12 @@
+package org.DEMO;
+
+public class Class {
+ private void data() {
+	// TODO Auto-generated method stub
+
+}
+	
+public static void main(String[] args) {
+	System.out.println("karthik ");
+}
+}
--- src/test/java/org/pomproject/ProjectClass.java
+++ src/test/java/org/pomproject/ProjectClass.java
@@ -7,8 +7,5 @@ public class ProjectClass extends BaseClass {
 public ProjectClass() {
 	PageFactory.initElements(driver, this);
 	
-	System.out.println("its me karthik here");
-	System.out.println("1111111111111111111");
-	
 }
 }
