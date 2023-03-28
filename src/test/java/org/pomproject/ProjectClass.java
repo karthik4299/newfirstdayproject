b1b1f9d4f2055225e2cfa6ccd92c5b4c5fdcafaa
Nishaanth23 <cbrnishaanth@gmail.com>
2023-03-28 09:25:33 +0530

    new

--- src/test/java/org/pomproject/ProjectClass.java
+++ src/test/java/org/pomproject/ProjectClass.java
@@ -5,16 +5,5 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.reporters.jq.Main;
 
 public class ProjectClass extends BaseClass {
-public ProjectClass() {
-	PageFactory.initElements(driver, this);
-	
-	System.out.println("1234567");
-	System.err.println("qwertyui");
-	
-	
-	
-}
-public static void main(String[] args) {
-	browserlaunch("chrome");
-}
+
 }
